53fe06cf673212e94a946bd6820bc0765b3b4dc5
NHW <nhw0926@gmail.com>
2025-05-11 00:49:04 +0900

    4일차 - router 및 레이아웃 router 사용용

---
+++ src/components/routes/RootLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import MainHeader from "../MainHeader";
+
+function RootLayout() {
+    return (
+        <>
+            <MainHeader />            
+            <Outlet />
+        </>
+    )
+}
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter  } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import NewPost from './components/NewPost.jsx'
+import RootLayout from './components/routes/RootLayout.jsx'
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <RootLayout />, 
+    children: [
+      { path: '/', element: <App />},
+      { path: '/create-post', element: <NewPost />},
+    ],
+  },  
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
